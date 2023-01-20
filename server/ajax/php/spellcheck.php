08922cece731d43ed948937debd4028cf5c0f796
moohoozaa <paramate.php@gmai.com>
2023-01-20 11:43:22 +0700

    first commit

---
+++ server/ajax/php/spellcheck.php
@@ -0,0 +1,27 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: *");
+header("Access-Control-Allow-Headers: *");
+if($post->params->lang){
+$RequestedLangs = explode(",",$post->params->lang);
+}else{
+$RequestedLangs= array("en");	
+}
+$spellcheckObject = new PHP_MCESpellCheck();
+$spellcheckObject -> LicenceKey=$token;
+$spellcheckObject -> IgnoreAllCaps = false;
+$spellcheckObject -> IgnoreNumeric = false;
+$spellcheckObject -> CaseSensitive = true;
+$spellcheckObject -> SuggestionTollerance = 1.5;
+$spellcheckObject -> DictionaryPath = ("../../dictionaries/");
+foreach($RequestedLangs as $dictionary){   $spellcheckObject -> LoadDictionary(trim($dictionary) );}
+$spellcheckObject -> LoadCustomDictionary("custom.txt");
+$spellcheckObject -> LoadCustomBannedWords("language-rules/banned-words.txt"); 
+$spellcheckObject -> LoadEnforcedCorrections("language-rules/enforced-corrections.txt");
+$spellcheckObject -> LoadCommonTypos("language-rules/common-mistakes.txt");
+
+function protect($word){
+	$word = str_replace("<","&lt;",$word);
+	$word = str_replace(">","&gt;",$word);	
+	return $word;
+}
